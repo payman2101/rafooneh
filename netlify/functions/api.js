4ccc5e217ab33b8af7ecff75943c68e98452342e
payman2101 <payman.2101@gmail.com>
2026-08-08 15:36:39 +0000

    Update netlify/functions/api.js serverless handler without comments

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,6 @@
 import serverless from 'serverless-http';
-// اگر فایل شما netlify/functions/api/index.js است، مسیر باید ../../server.js باشد
-// اگر فایل شما netlify/functions/api.js است، مسیر باید ../server.js باشد
-import app from '../../server.js'; 
-
+import app from '../../server.js';
 const serverlessHandler = serverless(app);
-
-// Netlify فقط export با نام handler را میشناسد
 export const handler = async (event, context) => {
-  return await serverlessHandler(event, context);
+return await serverlessHandler(event, context);
 };
