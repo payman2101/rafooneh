a39b3db0ade22e4ee334e933dc205395eb261ce3
payman2101 <payman.2101@gmail.com>
2026-08-08 15:46:08 +0000

    Update netlify/functions/api.js with Persian comments

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,11 @@
 import serverless from 'serverless-http';
-import app from '../../server.js';
+// توجه: چون فایل در netlify/functions/api.js است، برای رسیدن به ریشه باید دو مرحله بالا برویم
+import app from '../../server.js'; 
+
+// تبدیل برنامه اکسپرس به هندلر سرورلس
 const serverlessHandler = serverless(app);
+
+// نتلیفای فقط و فقط export با نام handler را میشناسد
 export const handler = async (event, context) => {
-return await serverlessHandler(event, context);
+  return await serverlessHandler(event, context);
 };
